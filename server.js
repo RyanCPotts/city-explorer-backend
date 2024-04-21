0e0633f7f5fdd0766d36952c2c512429d345e456
RyanCPotts <ryancpotts@gmail.com>
2024-04-21 16:26:47 -0500

    backend work

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const weatherData = require('./weather.json')
+
+// read the contents of the .env file into the "environment"
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Allow any client to make requests to this server
+app.use(cors());
+
+// home route
+// responds to requests for the / path
+// http://localhost:3000
+app.get('/', (request, response) => {
+  response.send('<h1>This is some html!</h1>');
+});
+
+app.get('/weather', getWeather)
+
+async function getWeather(request, response){
+    try{
+    const{lat, lon, searchQuery} = request.query;
+    console.log(lat, lon, searchQuery);
+    const findWeather = weatherData.find((city)=>{
+        return city.lat === lat || city.lon === lon || city.city_name.toLowerCase() === searchQuery
+    })
+
+    if(findWeather){
+        // response.status(200).json(findWeather)
+        console.log(findWeather)
+    }
+}   catch(e){
+    console.error(e.message)
+}
+}
+
+function startServer() {
+    let PORT = process.env.port || 3000;
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  }
+  
+  startServer();
