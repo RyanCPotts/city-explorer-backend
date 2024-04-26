dc97f32510289e5cd193db96b53f6c240d9cd308
RyanCPotts <ryancpotts@gmail.com>
2024-04-25 19:14:39 -0500

    city explorer backend complete

--- modules/weather.js
+++ modules/weather.js
@@ -1,5 +1,7 @@
 const weatherData = require('../weather.json')
 const axios = require('axios')
+let cache = require('./cache.js')
+// OUT CODE IS FROM LAB 07
 
 // class Forecast{
 //     constructor(weatherData){
@@ -9,13 +11,13 @@ const axios = require('axios')
 // }
 
 class Forecast {
-    constructor(weatherData) {
-      this.forecast = weatherData.weather.description;
-      this.low = weatherData.low_temp;
-      this.high = weatherData.high_temp;
-      this.date = weatherData.datetime;
-    }
+  constructor(weatherData) {
+    this.forecast = weatherData.weather.description;
+    this.low = weatherData.low_temp;
+    this.high = weatherData.high_temp;
+    this.date = weatherData.datetime;
   }
+}
 
 // async function getWeather(request, response){
 //     try{
@@ -38,24 +40,52 @@ class Forecast {
 // }
 
 async function getWeather(req, res) {
-    console.log(req, res)
-    try {
 
-   
-    let{lat, lon} = req.query;
+  // try {
+
+    let { lat, lon } = req.query;
+    const key = `${lat}:${lon}`;
+        if (!lat || !lon) {
+      return res.status(400).json({ error: 'please add a valid lat and lon' })
+    }
+    
+
+    if(cache[key]) {
+      let now = Date.now();
+      let timestamp = (now - cache[key].timestamp)/1000;
+      if(timestamp < 3600){
+        console.log('time passed', timeStamp)
+        console.log('cache data', cache[key])
+        res.json(cache[key].data);
+        return;
+      }
+      
+    }
+    console.log('no weather in cache')
+    // else {
+    //   console.log('Cache miss');
+    //   cache[key].timestamp = Date.now();
+    //   cache[key].data = 
+    // }
     let URL = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=5&units=I`;
 
     const axiosResponse = await axios.get(URL);
     let weatherData = axiosResponse.data;
 
-    let weather = weatherData.data.map( day => {
-        return new Forecast(day);
+    let weather = weatherData.data.map(day => {
+      return new Forecast(day);
     })
 
+    cache[key] = {
+      data: weather,
+      timestamp: Date.now()
+    }
+
+
     res.status(200).json(weather);
-}   catch(e){
-    res.status(500).json({e:'internal server error'})
-}
+  // } catch (e) {
+  //   res.status(500).json({ e: 'internal server error' })
+  // }
 }
 
 module.exports = getWeather;
